c83819b995394354fed82bb8b9d03e4384cec202
wangqiang <wangqiang@tianhecloud.com>
2022-09-06 20:43:14 +0800

    change execscript

--- execScript.go
+++ execScript.go
@@ -2,59 +2,71 @@ package main
 
 import (
 	"context"
-	//"bytes"
-	"log"
+
 	//"os"
 	logger "github.com/wonderivan/logger"
 	"os/exec"
-	"strings"
-	"syscall"
 )
+
+//const (
+//	ExampleShardID uint64 = 128
+//)
 
 var cancelFunc context.CancelFunc
 
-func ExecExternalScript(cmdTxt string) (string, string, error) {
-	//logger.Debug("Start execExternalScript! ")
-	//log.Printf("           ====================      start execExternalScript            =============")
+//func ExecExternalScript(cmdTxt string) (string, string, error) {
+//	//logger.Debug("Start execExternalScript! ")
+//	//log.Printf("           ====================      start execExternalScript            =============")
+//
+//	logger.Debug("start cmd:  [%s] !", cmdTxt)
+//
+//	ctx, cancel := context.WithCancel(context.Background())
+//	cancelFunc = cancel
+//
+//	//log.Printf("cmdTxt: %v", cmdTxt)
+//
+//	cmd := exec.CommandContext(ctx, "bash", "-c", cmdTxt)
+//	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+//	//cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+//	var stdout, stderr strings.Builder
+//	cmd.Stdout = &stdout
+//	cmd.Stderr = &stderr
+//	cmd.Start()
+//
+//	pgid, _ := syscall.Getpgid(cmd.Process.Pid)
+//	//var killFunc func() error
+//	killFunc := func() error {
+//		log.Printf("current pgid [%v]", pgid)
+//		return syscall.Kill(-pgid, syscall.SIGKILL) // note the minus sign
+//	}
+//	log.Printf("%v", killFunc())
+//
+//	if err := cmd.Wait(); err != nil {
+//		log.Printf("script cmd.wait(), err: %v", err)
+//	} else {
+//		//log.Printf("script finished!")
+//	}
+//
+//	//go func() {
+//	//	if err := cmd.Wait(); err != nil {
+//	//		fmt.Printf("Child process %d exit with err: %v\n", cmd.Process.Pid, err)
+//	//	}
+//	//}()
+//	//time.Sleep(1 * time.Second)
+//	//cmd.Process.Kill()
+//	log.Printf("              =================      execExternalScript finished" +
+//		"         ================")
+//
+//	return stdout.String(), stderr.String(), nil
+//}
 
-	logger.Debug("start exec %s.", cmdTxt)
-
+func ExecExternalScript(cmdTxt string) (string, error) {
+	logger.Debug("start cmd:  [%s] !", cmdTxt)
 	ctx, cancel := context.WithCancel(context.Background())
-	cancelFunc = cancel
-
-	//log.Printf("cmdTxt: %v", cmdTxt)
-
-	cmd := exec.CommandContext(ctx, "bash", "-c", cmdTxt)
-	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-	//cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-	var stdout, stderr strings.Builder
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	cmd.Start()
-
-	pgid, _ := syscall.Getpgid(cmd.Process.Pid)
-	//var killFunc func() error
-	killFunc := func() error {
-		log.Printf("current pgid [%v]", pgid)
-		return syscall.Kill(-pgid, syscall.SIGKILL) // note the minus sign
+	out, err := exec.CommandContext(ctx, "bash", "-c", cmdTxt).Output()
+	if err != nil {
+		panic(err)
 	}
-	log.Printf("%v", killFunc())
-
-	if err := cmd.Wait(); err != nil {
-		log.Printf("script cmd.wait(), err: %v", err)
-	} else {
-		//log.Printf("script finished!")
-	}
-
-	//go func() {
-	//	if err := cmd.Wait(); err != nil {
-	//		fmt.Printf("Child process %d exit with err: %v\n", cmd.Process.Pid, err)
-	//	}
-	//}()
-	//time.Sleep(1 * time.Second)
-	//cmd.Process.Kill()
-	log.Printf("              =================      execExternalScript finished" +
-		"         ================")
-
-	return stdout.String(), stderr.String(), nil
+	cancel()
+	return string(out), nil
 }
